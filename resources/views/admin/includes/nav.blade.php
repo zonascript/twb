6c5be24789a92c6c58708a1985062fe4bc7b964d
Pashamahardika <github@pashamahardika.com>
2017-05-23 01:55:37 +0000

    simplify the header nav

--- resources/views/admin/includes/nav.blade.php
+++ resources/views/admin/includes/nav.blade.php
@@ -9,14 +9,15 @@
 
     <div class="uk-navbar-right">
         <ul class="uk-navbar-nav">
-            <li><a>Hi, howdy  $user->name !</a></li>
+            {{--<li><a href="#">Hi, {!! \Auth::user()->name !!}</a></li>--}}
             <li>
-                <a class="uk-navbar-item" href="#">
-                    <img class="uk-border-circle" src="https://d2rbodpj0xodc.cloudfront.net/users/753534074538166240/avatar/84b19e98-5696-4a37-8283-4f3b532d2a01-150x150.jpeg" width="30" height="30" alt="username">
-                </a>
+                {{--<a class="uk-navbar-item" href="#">--}}
+                    {{--<img class="uk-border-circle" src="https://d2rbodpj0xodc.cloudfront.net/users/753534074538166240/avatar/84b19e98-5696-4a37-8283-4f3b532d2a01-150x150.jpeg" width="30" height="30" alt="username">--}}
+                {{--</a>--}}
+                <a href="#">Hi, {!! \Auth::user()->name !!}</a>
                 <div class="uk-navbar-dropdown uk-width-medium" uk-drop="offset: 0">
                     <ul class="uk-nav uk-navbar-dropdown-nav">
-                        <li><a href="{!! action('UserController@profile') !!}"><span class="uk-margin-small-right" uk-icon="icon: user"></span> My Profile</a></li>
+                        {{--<li><a href="{!! action('UserController@profile') !!}"><span class="uk-margin-small-right" uk-icon="icon: user"></span> My Profile</a></li>--}}
                         <li><a href="{!! url('/logout') !!}"><span class="uk-margin-small-right" uk-icon="icon: sign-out"></span> Logout</a></li>
                     </ul>
                 </div>
--- resources/views/admin/includes/sidenav.blade.php
+++ resources/views/admin/includes/sidenav.blade.php
@@ -23,26 +23,26 @@
             </ul>
         </li>
         <li class="uk-parent">
-            <a href="#"><span class="uk-margin-small-right" uk-icon="icon: paint-bucket"></span> Templates</a>
+            <a href="#"><span class="uk-margin-small-right" uk-icon="icon: image"></span> Templates</a>
             <ul class="uk-nav-sub">
                 <li><a href="{!! action('TemplateController@index') !!}">All Template</a></li>
                 <li><a href="{!! action('TemplateController@create') !!}">Upload</a></li>
             </ul>
         </li>
         <li class="uk-parent">
-            <a href="#"><span class="uk-margin-small-right" uk-icon="icon: star"></span> Campaign</a>
+            <a href="#"><span class="uk-margin-small-right" uk-icon="icon: paint-bucket"></span> Campaign</a>
             <ul class="uk-nav-sub">
                 <li><a href="#">Users</a></li>
                 <li><a href="#">Images</a></li>
             </ul>
         </li>
         <li class="uk-nav-divider"></li>
-        <li class="uk-parent">
-            <a href="#"><span class="uk-margin-small-right" uk-icon="icon: image"></span> Media</a>
-            <ul class="uk-nav-sub">
-                <li><a href="#">Images</a></li>
-            </ul>
-        </li>
+        {{--<li class="uk-parent">--}}
+            {{--<a href="#"><span class="uk-margin-small-right" uk-icon="icon: image"></span> Media</a>--}}
+            {{--<ul class="uk-nav-sub">--}}
+                {{--<li><a href="#">Images</a></li>--}}
+            {{--</ul>--}}
+        {{--</li>--}}
         <li class="uk-parent">
             <a href="#"><span class="uk-margin-small-right" uk-icon="icon: settings"></span> Setting</a>
             <ul class="uk-nav-sub">
