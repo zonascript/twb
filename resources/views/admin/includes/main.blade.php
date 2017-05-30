a998da112d85235b26ce756a7b49488b4d567223
oglab <n.nurjamal@gmail.com>
2017-05-31 04:37:33 +0700

    Toggle meta

--- resources/views/admin/includes/main.blade.php
+++ resources/views/admin/includes/main.blade.php
@@ -5,6 +5,7 @@
 <div class="uk-card k-border">
     <textarea class="uk-input textarea uk-width-1-1" name="content">@if(old('content') != ''){!! old('content') !!}@endif</textarea>
 </div>
+{{--
 <div class="uk-card uk-margin-top">
     <label>Excerpt</label>
     <textarea class="uk-textarea uk-width-1-1" name="excerpt" rows="5" placeholder="Enter Content Summary here">@if(old('excerpt') != ''){!! old('excerpt') !!}@endif</textarea>
@@ -18,3 +19,25 @@
     <label>Meta Description</label>
     <textarea class="uk-textarea uk-width-1-1" name="meta_description" placeholder="Enter meta description here">@if(old('meta_description') != ''){!! old('meta_description') !!}@endif</textarea>
 </div>
+--}}
+<ul uk-accordion="multiple: true">
+    <li class="uk-open">
+        <h5 class="uk-accordion-title">Excerpt</h5>
+        <div class="uk-accordion-content uk-margin-small-top">
+            <textarea class="uk-textarea uk-width-1-1" name="excerpt" rows="5" placeholder="Enter Content Summary here">@if(old('excerpt') != ''){!! old('excerpt') !!}@endif</textarea>
+        </div>
+    </li>
+    <li class="uk-open">
+        <h5 class="uk-accordion-title">Page Title</h5>
+        <div class="uk-accordion-content uk-margin-small-top">
+            <input class="uk-input uk-width-1-1" type="text" name="page_title" placeholder="Enter page title here"
+               @if(old('page_title') != '')value="{!! old('page_title') !!}"@endif />
+        </div>
+    </li>
+    <li class="uk-open">
+        <h5 class="uk-accordion-title">Meta Description</h5>
+        <div class="uk-accordion-content uk-margin-small-top">
+            <textarea class="uk-textarea uk-width-1-1" name="meta_description" placeholder="Enter meta description here">@if(old('meta_description') != ''){!! old('meta_description') !!}@endif</textarea>
+        </div>
+    </li>
+</ul>
