f34ba06aafe8d59d7c58291be16c24e048da215f
Pashamahardika <github@pashamahardika.com>
2017-09-05 10:06:10 +0000

    feedback 20170905

--- resources/views/duniamain/term.blade.php
+++ resources/views/duniamain/term.blade.php
@@ -33,7 +33,7 @@
                         <li>Buka aplikasinya, lalu ajak Si Kecil memainkan permainan “Dunia Main Tini Wini Biti” dengan men-scan
                             koin Tini Wini Biti terlebih dahulu;</li>
                         <li>Mainkan hingga mendapat skor tertinggi, lalu ambil foto seru Moms bersama Si Kecil sesuai karakter koin yang dimiliki;</li>
-                        <li>Upload foto Moms bersama Si Kecil pada di www.tiniwinibiti.id/duniamain;</li>
+                        <li>Upload foto Moms bersama Si Kecil pada di <a href="www.tiniwinibiti.id/duniamain" title="www.tiniwinibiti.id/duniamain" class="dm-red-text">www.tiniwinibiti.id/duniamain</a>;</li>
                         <li>Setiap akun Facebook yang terdaftar akan memiliki album foto sebanyak 15 yang dapat digunakan untuk upload foto.</li>
                         <li>Moms disarankan untuk melengkapi ke-15 koin Tini Wini Biti untuk mendapat kesempatan memenangkan grand prizes;</li>
                         <li>Undang teman-teman Moms sebanyak mungkin untuk berpartisipasi dalam kompetisi Dunia Main Tini Wini Biti.</li>
@@ -45,7 +45,7 @@
                         <li>Peserta adalah Warga Negara Indonesia.</li>
                         <li>Peserta harus mempunyai akun Facebook untuk bisa mengikuti Kontes Dunia Main Tini Wini Biti</li>
                         <li>Jangan lupa untuk melengkapi formulir registrasi dengan data yang benar lengkap untuk mendapatkan kesempatan menjadi pemenang;</li>
-                        <li>Kontes Dunia Main Tini Wini Biti dimulai dari tanggal 14 Agustus 2017 dan berakhir pada tanggal 18 Oktober 2017</li>
+                        <li>Kompetisi Dunia Main Tini Wini Biti dimulai dari tanggal 6 September 2017 dan berakhir pada tanggal 8 November 2017</li>
 
                         <li>KETENTUAN PEMENANG
                             <ul>
@@ -57,8 +57,8 @@
 
                         <li>PENGUMUMAN PEMENANG
                         <ul>
-                            <li>Pemenang Grand Prize dan Hadiah Hiburan akan diumumkan pada tanggal 26 Oktober 2017.</li>
-                            <li>Pemenang Weekly Challenge akan diumumkan setiap hari selasa di setiap minggunya selama berjalannya kompetisi.</li>
+                            <li>Pemenang Grand Prize dan Hadiah Hiburan akan diumumkan pada tanggal 15 November 2017.</li>
+                            <li>Pemenang Weekly Challenge akan diumumkan setiap hari rabu di setiap minggunya selama berjalannya kompetisi.</li>
                         </ul>
                         </li>
 
@@ -67,24 +67,27 @@
                                 <li>GRAND PRIZE
                                     <ul>
                                         <li>1 (satu) unit Play Station Vita</li>
+                                        <li>1 (satu) unit Samsung Tablet</li>
+                                        <li>3 (tiga) unit Xiaomi Smartphone</li>
                                     </ul>
                                 </li>
                                 <li>HADIAH HIBURAN
                                     <ul>
-                                        <li>1 (satu) unit Action Camera</li>
-                                        <li>1 (satu) unit Samsung Tablet</li>
-                                        <li>1 (satu) unit Lego Set</li>
+                                        <li>5 (lima) unit Lego Set</li>
+                                        <li>10 (sepuluh) unit Drawing Supplies</li>
+                                        <li>20 (dua puluh) unit Toy Boxes</li>
                                     </ul>
                                 </li>
-                                <li>HADIAH MINGGUAN ( REVISI )
+                                <li>HADIAH MINGGUAN
                                     <ul>
-                                        <li>21 Agustus - 27 Agustus 2017 (    hadiah      )</li>
-                                        <li>28 Agustus - 1 September 2017 (    hadiah         )</li>
-                                        <li>4 September - 8 September 2017 (     hadiah           )</li>
-                                        <li>11 September - 17 September 2017 (     hadiah       )</li>
-                                        <li>18 September - 22 September 2017 (       hadiah     )</li>
-                                        <li>2 Oktober - 6 Oktober 2017 (    hadiah     )</li>
-                                        <li>9 Oktober - 13 Oktober 2017 (      hadiah     )</li>
+                                        <li>6 September – 17 September 2017 (Voucher belanja)</li>
+                                        <li>18 September - 24 September 2017 (Voucher belanja)</li>
+                                        <li>25 September - 1 Oktober 2017 (Voucher belanja)</li>
+                                        <li>2 Oktober – 8 Oktober 2017 (Voucher belanja)</li>
+                                        <li>9 Oktober - 15 Oktober 2017 (Voucher belanja)</li>
+                                        <li>23 Oktober - 29 Oktober 2017 (Voucher belanja)</li>
+                                        <li>30 Oktober – 5 November 2017 (Voucher belanja)</li>
+                                        <li>Pemenang mingguan mendapat voucher belanja masing-masing Rp 200.000</li>
                                     </ul>
                                 </li>
                             </ol>
